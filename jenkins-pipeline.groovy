0ea941a489cf949cd44e78cf7c846f00d88238e2
Francisco Junio <francisco.junio.prestador@prf.gov.br>
2020-09-16 16:45:01 -0300

    tt

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,173 +1,23 @@
-pipeline {
+node('master') {
+    checkout scm
+    stage('approval') {
+        timeout(time: 30, unit: 'DAYS') {
+                input message: "Start first rollout ?"
+            }
+    }
+    stage('first rollout') {    
+    openshift.withCluster( 'mycluster' ) {
+    echo "Hello from ${openshift.cluster()}'s default project: ${openshift.project()}"
 
-  agent {
-
-      label 'maven'
-
-  }
-
-  stages {
-
-    stage('Build App') {
-
-      steps {
-
-        sh "mvn install"
-
-      }
-
+    // But we can easily change project contexts
+    openshift.withProject( 'another-project' ) {
+        echo "Hello from a non-default project: ${openshift.project()}"
     }
 
-    stage('Create Image Builder') {
-
-      when {
-
-        expression {
-
-          openshift.withCluster() {
-
-            return !openshift.selector("bc", "mapit").exists();
-
-          }
-
-        }
-
-      }
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.newBuild("--name=mapit", "--image-stream=redhat-openjdk18-openshift:1.1", "--binary")
-
-          }
-
-        }
-
-      }
-
+    // And even scope operations to other clusters within the same script
+    openshift.withCluster( 'myothercluster' ) {
+        echo "Hello from ${openshift.cluster()}'s default project: ${openshift.project()}"
     }
-
-    stage('Build Image') {
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.selector("bc", "mapit").startBuild("--from-file=target/mapit-spring.jar", "--wait")
-
-          }
-
-        }
-
-      }
-
-    }
-
-    stage('Promote to DEV') {
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.tag("mapit:latest", "mapit:dev")
-
-          }
-
-        }
-
-      }
-
-    }
-
-    stage('Create DEV') {
-
-      when {
-
-        expression {
-
-          openshift.withCluster() {
-
-            return !openshift.selector('dc', 'mapit-dev').exists()
-
-          }
-
-        }
-
-      }
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.newApp("mapit:latest", "--name=mapit-dev").narrow('svc').expose()
-
-          }
-
-        }
-
-      }
-
-    }
-
-    stage('Promote STAGE') {
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.tag("mapit:dev", "mapit:stage")
-
-          }
-
-        }
-
-      }
-
-    }
-
-    stage('Create STAGE') {
-
-      when {
-
-        expression {
-
-          openshift.withCluster() {
-
-            return !openshift.selector('dc', 'mapit-stage').exists()
-
-          }
-
-        }
-
-      }
-
-      steps {
-
-        script {
-
-          openshift.withCluster() {
-
-            openshift.newApp("mapit:stage", "--name=mapit-stage").narrow('svc').expose()
-
-          }
-
-        }
-
-      }
-
-    }
-
-  }
-
+}
+}
 }
